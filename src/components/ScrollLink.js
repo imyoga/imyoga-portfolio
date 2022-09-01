bb4b1d1d12ac7091cd49ce2a81865402318570a8
yogesh_git <moradiyayogeshg@gmail.com>
2022-09-01 12:52:41 -0400

    re-coloring done

--- src/components/ScrollLink.js
+++ src/components/ScrollLink.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import { Link } from 'react-scroll';
 import * as Scroll from 'react-scroll';
-import './Navbar';
+import './ScrollLink.css';
+import './Navbar.css';
+
 
 const ScrollLink = (props) => {
-
   // console.log(props);
 
   function clickHandler() {
-    console.log("clicked on scroll link");
-    if (props.ontoggle){
+    console.log('clicked on scroll link');
+    if (props.ontoggle) {
       props.ontoggle();
     }
 
     if (props.scrollToTop === 1) {
       Scroll.animateScroll.scrollToTop();
-    } 
+    }
   }
 
   return (
     <Link
       activeClass="active"
       className={props.classes}
-      to={props.scrollTo==="Hero" ? null:props.scrollTo}
+      to={props.scrollTo === 'Hero' ? null : props.scrollTo}
       spy={true}
       smooth={true}
       offset={props.offset ?? -56}
@@ -35,5 +36,3 @@ const ScrollLink = (props) => {
 };
 
 export default ScrollLink;
-
-
